cc63f2ea6a7d21e515323728b86c3f6f36e957da
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-02 21:26:08 +0300

    fix: throw when given bad webContents

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const electron = require('electron')
 const uuid = require('uuid')
+const assert = require('assert')
 
 module.exports = function (webContents, val) {
+  assert(
+    typeof webContents.executeJavaScript === 'function',
+    `${webContents} is not a webContents object`
+  )
   return new Promise((resolve, reject) => {
     const eventId = uuid.v4()
     const ipcMain = electron.ipcMain
